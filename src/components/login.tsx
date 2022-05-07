91af83bd8e764fd4e022604f840ccbb3c89cd2e9
jonatan <jonatan.haaralaorosco@gmail.com>
2022-05-06 23:47:21 -0300

    changed login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,11 +1,13 @@
-import {Component} from 'react';
+import {Component, SyntheticEvent} from 'react';
 import { AuthService } from '../services/authService';
 
 //interfaces
 interface LoginProps {
     authService: AuthService
 }
-
+interface CustomEvent {
+    target: HTMLInputElement
+}
 interface LoginState{
     userName: string,
     password: string,
@@ -22,16 +24,39 @@ export class Login extends Component <LoginProps, LoginState>{
         isSuccesfull: false
     };
 
+    private setuserName(e: CustomEvent){
+        this.setState({userName: e.target.value})
+    }
+    private setPassword(e: CustomEvent){
+        this.setState({password: e.target.value})
+    }
+    private async handleSubmit(e: SyntheticEvent){
+        e.preventDefault()
+        const result = await this.props.authService.login(
+            this.state.userName,
+            this.state.password
+        )
+        result ? console.log(result) : console.error();
+    }
     render(){
         return(
             <div>
                 <h1>Please login</h1>
-                <form action="post">
-                    <input type="text" value={this.state.userName}/><br />
-                    <input type="password" value={this.state.password} /><br />
+
+                <form onSubmit={e => this.handleSubmit(e)}>
+                    <input 
+                        type="text" 
+                        onChange={e => this.setuserName(e)} 
+                        value={this.state.userName}
+                    /><br />
+                    <input 
+                        type="password" 
+                        onChange={e => this.setPassword(e)} 
+                        value={this.state.password} 
+                    /><br />
                     <input type="submit" value="login" />
-                    
                 </form>
+
             </div>
         );
     }
